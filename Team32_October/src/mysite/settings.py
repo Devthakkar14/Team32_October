52541391f0b742aa962590c9b9550b746e124c72
dev20052 <dev20052@iiitd.ac.in>
2022-11-27 12:30:34 +0000

    hosting

--- Team32_October/src/mysite/settings.py
+++ Team32_October/src/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-9uma+a1aa(^gsvb1-ul8$++y_v(lb6g^2fg(-n6j^y*q#+15hk"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
